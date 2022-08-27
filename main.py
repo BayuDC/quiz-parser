99e1c1c867c9be1e1c40d99f1df774ae2f73baec
BayuDC <dev.bayudc@gmail.com>
2022-08-27 21:19:39 +0700

    chore: automatically load file

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
 import docx
 import yaml
+import glob
 
-document = docx.Document('./src/sample.docx')
 
-quiz = {
-    'name': '',
-    'questions': [],
-}
+def convert(filename):
+    document = docx.Document(filename)
 
-for p in document.paragraphs:
-    if p.style.name == 'Heading 1':
-        quiz['name'] = p.text
-    elif p.style.name == 'Heading 2':
-        quiz['questions'].append({
-            'body': p.text,
-            'choices': []
-        })
-    elif p.style.name == 'normal':
-        correct = False
-        for r in p.runs:
-            if r.bold == True:
-                correct = True
-                break
+    quiz = {
+        'name': '',
+        'questions': [],
+    }
 
-        quiz['questions'][-1]['choices'].append({
-            'body': p.text,
-            'correct': correct
-        })
+    for p in document.paragraphs:
+        if p.style.name == 'Heading 1':
+            quiz['name'] = p.text
+        elif p.style.name == 'Heading 2':
+            quiz['questions'].append({
+                'body': p.text,
+                'choices': []
+            })
+        elif p.style.name == 'normal':
+            correct = False
+            for r in p.runs:
+                if r.bold == True:
+                    correct = True
+                    break
 
-with open('./dist/sample.yaml', 'w') as file:
-    yaml.dump(quiz, file)
+            quiz['questions'][-1]['choices'].append({
+                'body': p.text,
+                'correct': correct
+            })
+
+    with open(filename.replace('src', 'dist').replace('docx', 'yml'), 'w') as file:
+        yaml.dump(quiz, file)
+
+
+for filename in glob.glob('./src/*.docx'):
+    convert(filename)
