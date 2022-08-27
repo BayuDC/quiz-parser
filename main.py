ba22960161b15a68f8803009e9dc8ae2f203e16d
BayuDC <dev.bayudc@gmail.com>
2022-08-27 18:49:48 +0700

    feat: parse the quiz questions

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from docx import Document
 
 document = Document('./src/sample.docx')
 
-quiz = {}
+quiz = {
+    'name': '',
+    'questions': [],
+}
 
 for p in document.paragraphs:
     if p.style.name == 'Heading 1':
         quiz['name'] = p.text
+    elif p.style.name == 'Heading 2':
+        quiz['questions'].append({
+            'body': p.text
+        })
 
 print(quiz)
