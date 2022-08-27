2887a2672f7ba866e24f3d112b9b863d86af4bad
BayuDC <dev.bayudc@gmail.com>
2022-08-27 18:47:05 +0700

    feat: parse the quiz name

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hello world')
+from docx import Document
+
+document = Document('./src/sample.docx')
+
+quiz = {}
+
+for p in document.paragraphs:
+    if p.style.name == 'Heading 1':
+        quiz['name'] = p.text
+
+print(quiz)
